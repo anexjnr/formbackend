4f2d640c4e18c270ecdcdc3d2efba8fbff42c729
anexjnr <axpauly@gmail.com>
2024-02-09 11:36:10 +0530

    security changes

--- route.js
+++ route.js
@@ -1,20 +1,31 @@
 const express=require("express")
 const router = express.Router()
+const bcrypt = require("bcryptjs")
 
 const formmodel = require("./model")
 
+hashPasswordGenerator=async(password)=>{
+    const salt = await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
+}
 
 router.post("/add",async (req,res)=>
 {
-let data = req.body
+let {data} = {"data": req.body}
+let password=data.password
+hashPasswordGenerator(password).then(
+    (hashedPassword)=>
+    {
+console.log(hashedPassword)
+data.password=hashedPassword
+console.log(data)
 let form = new formmodel(data)
-let result = await form.save()
+let result = form.save()
 res.json(
     {
         status:"success"
     }
 )
+    })
 })
-
-
 module.exports=router
